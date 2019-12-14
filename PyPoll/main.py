acd5d41205524dec5a9b5ecb42e65dbae0539271
carlreim <carlreim@gmail.com>
2019-12-14 16:39:28 -0600

    added more comments and deleted useless code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,43 +2,52 @@
 import os, csv
 from collections import Counter
 
+
+# create various lists
+candidates = []    #list of all items in candidates column
+votes = []
+votesPCT = []
+
 # open the csv data file
 poll_path = os.path.join("election_data.csv")
 
-
-candidates = []
-numVote = 0
 with open(poll_path, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     # find header, skip header
     header = next(csvreader)
-    print(header)
+    # add all votes into list
     for row in csvreader:
         candidates.append(row[2])
+
+# get total number of votes
 numVotes = len(candidates)
-set = set(candidates)
-uniqueCandidates = (list(set))
+# create a set of candidates and then a list of unique candidates
+
+# set = set(candidates)
+# uniqueCandidates = (list(set))
+
+# use Counter to create a dictionary of canidates and number of
+# times they occur in the list / votes they recieved
 resultsDict = Counter(candidates)
 
+# get a list of the keys within the dictionary, this is a list of unique Candidates
+keys = list(resultsDict)
+# return the length of unique candidates
+lenKeys = len(keys)
+
+# print some structure and context
 print(f'Election results')
 print(f'<><><><><><><><><><><><><><><>')
 print(f'Total votes: {numVotes}')
 print(f'<><><><><><><><><><><><><><><>')
-#print(uniqueCandidates)
-for x in uniqueCandidates:
-    print(x)
-print(resultsDict)
-keys = list(resultsDict)
-lenKeys = len(keys)
-votes = []
-votesPCT = []
+
+# for each unique candidate, take the number of votes recieved and put into another list.
+# then, calculate the percentage. Print the results for each candidate
 for y in range(lenKeys):
     votes.append(resultsDict[keys[y]])
     votesPCT.append(round(float(resultsDict[keys[y]]/numVotes)*100,2))
     print(f'Candidate: {keys[y]} | Votes: {votes[y]} | Percentage: {votesPCT[y]}') #float(resultsDict[keys[y]])
+# find the max vote value and return the candidate and percentage in the same list position
+print(f'<><><><><><><><><><><><><><><>')
 maxVotesPos = votes.index(max(votes))
-
 print(f'The winner is {keys[maxVotesPos]} with {votesPCT[maxVotesPos]} percent of the votes')
-# print(votes)
-# print(maxVotesPos)
-# print(votesPCT)
