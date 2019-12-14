2f3d5b6dfc52a49b01590868ff2b8753e94a355d
carlreim <carlreim@gmail.com>
2019-12-14 16:19:18 -0600

    Finished with main work, need to refine

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,29 +1,44 @@
 # import modules
 import os, csv
-
+from collections import Counter
 
 # open the csv data file
 poll_path = os.path.join("election_data.csv")
 
 
 candidates = []
-#numVotes = []
+numVote = 0
 with open(poll_path, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     # find header, skip header
     header = next(csvreader)
     print(header)
-    numVote = 0
     for row in csvreader:
         candidates.append(row[2])
 numVotes = len(candidates)
 set = set(candidates)
 uniqueCandidates = (list(set))
+resultsDict = Counter(candidates)
 
 print(f'Election results')
 print(f'<><><><><><><><><><><><><><><>')
 print(f'Total votes: {numVotes}')
 print(f'<><><><><><><><><><><><><><><>')
-print(uniqueCandidates)
+#print(uniqueCandidates)
 for x in uniqueCandidates:
     print(x)
+print(resultsDict)
+keys = list(resultsDict)
+lenKeys = len(keys)
+votes = []
+votesPCT = []
+for y in range(lenKeys):
+    votes.append(resultsDict[keys[y]])
+    votesPCT.append(round(float(resultsDict[keys[y]]/numVotes)*100,2))
+    print(f'Candidate: {keys[y]} | Votes: {votes[y]} | Percentage: {votesPCT[y]}') #float(resultsDict[keys[y]])
+maxVotesPos = votes.index(max(votes))
+
+print(f'The winner is {keys[maxVotesPos]} with {votesPCT[maxVotesPos]} percent of the votes')
+# print(votes)
+# print(maxVotesPos)
+# print(votesPCT)
